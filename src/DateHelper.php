9e91a62c18c9937d2e36272dccba11aa50f1ff0d
denis.vasiliev <dv@cybetic.com>
2025-12-05 11:35:14 +0200

    BP-4963

--- src/DateHelper.php
+++ src/DateHelper.php
@@ -73,6 +73,7 @@ class DateHelper
                 $start->startOfYear();
                 break;
             default:
+                $value = str_replace(' to ', ' - ', $value);
                 $parsed = explode(' - ', $value);
 
                 if (count($parsed) === 1) {
